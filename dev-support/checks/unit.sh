fe9ff829e0b51deef1003d4189483972f3afc6e5
Doroszlai, Attila <adoroszlai@apache.org>
2021-12-03 16:07:36 +0100

    Allow running tests repeatedly

--- dev-support/checks/unit.sh
+++ dev-support/checks/unit.sh
@@ -14,22 +14,60 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -o pipefail
+set -u -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR/../.." || exit 1
 
+: ${ITERATIONS:="1"}
+
+declare -i ITERATIONS
+if [[ ${ITERATIONS} -le 0 ]]; then
+  ITERATIONS=1
+fi
+
+export MAVEN_OPTS="-Xmx4096m $MAVEN_OPTS"
+MAVEN_OPTIONS='-B'
+
+if [[ "${FAIL_FAST:-}" == "true" ]]; then
+  MAVEN_OPTIONS="${MAVEN_OPTIONS} --fail-fast -Dsurefire.skipAfterFailureCount=1"
+else
+  MAVEN_OPTIONS="${MAVEN_OPTIONS} --fail-at-end"
+fi
+
+if [[ ${ITERATIONS} -gt 1 ]]; then
+  mvn ${MAVEN_OPTIONS} -DskipTests clean install
+fi
+
 REPORT_DIR=${OUTPUT_DIR:-"$DIR/../../target/unit"}
 mkdir -p "$REPORT_DIR"
 
-export MAVEN_OPTS="-Xmx4096m"
-mvn -B -fae test "$@" | tee "${REPORT_DIR}/output.log"
-rc=$?
+rc=0
+for i in $(seq 1 ${ITERATIONS}); do
+  if [[ ${ITERATIONS} -gt 1 ]]; then
+    original_report_dir="${REPORT_DIR}"
+    REPORT_DIR="${original_report_dir}/iteration${i}"
+    mkdir -p "${REPORT_DIR}"
+  fi
 
-# shellcheck source=dev-support/checks/_mvn_unit_report.sh
-source "$DIR/_mvn_unit_report.sh"
+  mvn ${MAVEN_OPTIONS} "$@" test \
+    | tee "${REPORT_DIR}/output.log"
+  irc=$?
 
-if [[ -s "$REPORT_DIR/summary.txt" ]] ; then
-    exit 1
-fi
+  # shellcheck source=hadoop-ozone/dev-support/checks/_mvn_unit_report.sh
+  source "${DIR}/_mvn_unit_report.sh"
+  if [[ ${irc} == 0 ]] && [[ -s "${REPORT_DIR}/summary.txt" ]]; then
+    irc=1
+  fi
+
+  if [[ ${ITERATIONS} -gt 1 ]]; then
+    REPORT_DIR="${original_report_dir}"
+    echo "Iteration ${i} exit code: ${irc}" | tee -a "${REPORT_DIR}/summary.txt"
+  fi
+
+  if [[ ${rc} == 0 ]]; then
+    rc=${irc}
+  fi
+done
+
 exit ${rc}
